716d77f8cf252891758ba67da512fc735902de58
Golu <webdeveloper@jigsaw.edu.in>
2023-12-02 15:15:17 +0530

    last

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,7 @@ const mongoose =  require('mongoose');
 
 // mongodb://localhost:27017
 // mongodb+srv://webdeveloper1:IK9ez3LQ91YwQU7F@cluster0.6qnw3vh.mongodb.net/myDatabase?retryWrites=true&w=majority'
-mongoose.connect('mongodb://localhost:27017/mydatabase',{
+mongoose.connect('mongodb+srv://webdeveloper1:IK9ez3LQ91YwQU7F@cluster0.6qnw3vh.mongodb.net/myDatabase?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(() => {
