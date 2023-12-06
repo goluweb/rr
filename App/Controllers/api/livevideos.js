a5719c05a01dfcd3a5d90728c89ee4ae11cd994a
goluweb <114050414+goluweb@users.noreply.github.com>
2023-12-06 13:21:06 +0530

    Update livevideos.js

--- App/Controllers/api/livevideos.js
+++ App/Controllers/api/livevideos.js
@@ -2,99 +2,86 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 
-const CLIENT_ID = '2QzXoasoQBimWPtbXIsg';
-const CLIENT_SECRET = '7JP72COlmOyFbFjSOETepQr3eyADgp4G';
-const crypto = require('crypto');
+const clientId = '2QzXoasoQBimWPtbXIsg';
+const clientSecret = '7JP72COlmOyFbFjSOETepQr3eyADgp4G';
+const redirectUri = 'https://node-project-rc3o.onrender.com/api/live/callback';
 
+// Function to refresh the Zoom access token using the refresh token
+async function refreshAccessToken(refreshToken) {
+  try {
+    const response = await axios.post('https://zoom.us/oauth/token', null, {
+      params: {
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+        client_id: clientId,
+        client_secret: clientSecret,
+      },
+    });
+    return response.data.access_token;
+  } catch (error) {
+    console.error('Error refreshing access token:', error);
+    throw error;
+  }
+}
 
-// Function 
-
-
-const querystring = require('querystring');  
 app.get('/authorize', (req, res) => {
-  const redirectUri = encodeURIComponent('https://node-project-rc3o.onrender.com/api/live/callback');
-  const state = crypto.randomBytes(16).toString('hex');
-  const authorizeUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${redirectUri}&state=${state}`;
-  res.redirect(authorizeUrl);
+  res.redirect(`https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`);
 });
 
 app.get('/callback', async (req, res) => {
   const code = req.query.code;
-  // Exchange the code for an access token
-  const tokenParams = {
-    grant_type: 'authorization_code',
-    code,
-    redirect_uri: 'https://node-project-rc3o.onrender.com/api/live/callback', // Must match the redirect URI you used in the authorize request
-  };
 
-  const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  };
-  const authString = `${CLIENT_ID}:${CLIENT_SECRET}`;
-  const base64Auth = Buffer.from(authString).toString('base64');
-  headers.Authorization = `Basic ${base64Auth}`;
   try {
-    const response = await axios.post('https://zoom.us/oauth/token', querystring.stringify(tokenParams), { headers });
-    const accessToken = response.data.access_token;
-    console.log('Access Token:', accessToken);
-    console.log('tt:', response.data);
-    res.send('Access Token: ' + accessToken);
-     } 
-    catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).send('Error');
+    const tokenResponse = await axios.post('https://zoom.us/oauth/token', null, {
+      params: {
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri,
+        client_id: clientId,
+        client_secret: clientSecret,
+      },
+    });
+
+    const accessToken = tokenResponse.data.access_token;
+    const refreshToken = tokenResponse.data.refresh_token;
+    console.log(accessToken)
+    console.log("refresh"+refreshToken)
+    // Save the refresh token securely for future use
+
+    // Redirect to a success page or handle as needed
+    res.send('Authorization successful');
+  } catch (error) {
+    console.error('Error during OAuth callback:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
-
-
-
-
-
-
-
 app.post('/golive', async (req, res) => {
   try {
-    await ensureValidToken();
+    // Replace <your_refresh_token> with the actual refresh token obtained during OAuth
+    const refreshToken = 'eyJzdiI6IjAwMDAwMSIsImFsZyI6IkhTNTEyIiwidiI6IjIuMCIsImtpZCI6ImNlODg1YmUyLWJlNTAtNDZhNy1iOWEyLWY2YTRmNjBiNzA4OCJ9.eyJ2ZXIiOjksImF1aWQiOiJhZGNhOThiNzM4OGVjYzg4OThmNmViZmU2MGI3ODZmNyIsImNvZGUiOiJEV2xSTWhjc3dRZEFVRmZCdGQxUTltaU1ycjFVWTh1X0EiLCJpc3MiOiJ6bTpjaWQ6MlF6WG9hc29RQmltV1B0YlhJc2ciLCJnbm8iOjAsInR5cGUiOjEsInRpZCI6MCwiYXVkIjoiaHR0cHM6Ly9vYXV0aC56b29tLnVzIiwidWlkIjoiUS03VkxIaWVSLVdNdThTU292M2NqQSIsIm5iZiI6MTcwMTg0ODY3OCwiZXhwIjoxNzA5NjI0Njc4LCJpYXQiOjE3MDE4NDg2NzgsImFpZCI6ImZCTFVGS3JHUldXZVg1LTA3TWRQb2cifQ.9B3rBRbV-AIODuXvbukCENRyKQEMS-us_cf20xMliuM8xOwBXpwoTSnl_X-GpaXFV3cDG3yHMRkxII8gprUGQg';
+    const accessToken = await refreshAccessToken(refreshToken);
 
     const meetingParams = {
       host_id: 'me',
-      topic: 'Arvind Pahadi',
-      type: 2, 
+      topic: 'dinesh Pahadi',
+      type: 2,
       start_time: '2023-08-05T12:00:00Z',
       timezone: 'IST',
     };
-    let BEARER_TOKEN ='eyJzdiI6IjAwMDAwMSIsImFsZyI6IkhTNTEyIiwidiI6IjIuMCIsImtpZCI6IjJlMjdkY2M1LTE0NmYtNDc5Ni1iYzcyLWM0MjEwYTMwMzZjZCJ9.eyJ2ZXIiOjksImF1aWQiOiJhZGNhOThiNzM4OGVjYzg4OThmNmViZmU2MGI3ODZmNyIsImNvZGUiOiJZYTJNOUVGMldCNHZoUjhFRDk3UXJPazNuN1k0a3lTTlEiLCJpc3MiOiJ6bTpjaWQ6MlF6WG9hc29RQmltV1B0YlhJc2ciLCJnbm8iOjAsInR5cGUiOjEsInRpZCI6MCwiYXVkIjoiaHR0cHM6Ly9vYXV0aC56b29tLnVzIiwidWlkIjoiUS03VkxIaWVSLVdNdThTU292M2NqQSIsIm5iZiI6MTY5NjkyNDYxNSwiZXhwIjoxNzA0NzAwNjE1LCJpYXQiOjE2OTY5MjQ2MTUsImFpZCI6ImZCTFVGS3JHUldXZVg1LTA3TWRQb2cifQ.vRTH3pgEK5TsJqVWVt1QraKHNJeFK-ciw91_zDXojsxpZnP6fw-HYUS_XSejTXuT782792LqNppXssaulL4b1g';
 
     const response = await axios.post('https://api.zoom.us/v2/users/me/meetings', meetingParams, {
       headers: {
-        'Authorization': `Bearer ${BEARER_TOKEN}`,
+        'Authorization': `Bearer ${accessToken}`,
       },
     });
 
     console.log('Meeting created:', response.data);
     res.json(response.data);
   } catch (error) {
-    console.error('Error creating meeting:', error.response.data);
-    res.status(error.response.status).json(error.response.data);
+    console.error('Error creating meeting:', error);
+    res.status(error.response?.status || 500).json(error.response?.data || { message: 'Internal Server Error' });
   }
 });
 
-
-
-
-// Define a route to generate the Zoom signature
-app.get('/generate-signature', (req, res) => {
-
-  const apiKey = 'Pm0aZWzoS0C6YKxtlUFEJw';
-  const apiSecret = 'HhJR497swnjN84mn3RowBT8vprBf8mUw';
-
-  const timestamp = new Date().getTime() - 30000; // 30 seconds in the past
-  const message = Buffer.from(apiKey + req.query.meetingNumber + timestamp + req.query.role).toString('base64');
-  const signature = crypto.createHmac('sha256', apiSecret).update(message).digest('base64');
-
-  res.json({ signature });
-});
-
-
-module.exports=app;
+module.exports = app;
